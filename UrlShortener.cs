4e83aaed2b2edc3b7d174f21cba5ff55bfab8fe9
Franky Mac <danielfrankga92@gmail.com>
2023-05-23 09:50:23 +0200

    initial commit

---
+++ UrlShortener.cs
@@ -0,0 +1,5 @@
+namespace UrlShortener;
+
+public class UrlShortener
+{
+}
---
+++ UrlShortenerTest.cs
@@ -0,0 +1,10 @@
+namespace UrlShortener;
+
+public class UrlShortenerTest
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
